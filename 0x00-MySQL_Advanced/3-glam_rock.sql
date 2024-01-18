333999d56219bf169924e311fa29c01ee4dcaad2
Kingvadee <ojeifod8@gmail.com>
2024-01-17 18:42:26 -0800

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,11 @@
--- This SQL script select band_name, and lifespan column which is difference
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- This SQL script selects band_name and lifespan columns, calculating the difference
+SELECT
+    band_name,
+    (IFNULL(split, '2020') - formed) AS lifespan
+FROM
+    metal_bands
+WHERE
+    FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY
+    lifespan DESC;
+

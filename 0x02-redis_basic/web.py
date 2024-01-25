687108b82265af22cdba9ed696ac564450ad5c2f
kingvadee <ojeifod8@gmail.com>
2024-01-25 01:48:07 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,20 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+'''Reading from Redis and recovering original type'''
+
+
 import redis
-import requests
-from functools import wraps
-from typing import Callable
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+class Cache:
+    '''Cache class'''
+    def __init__(self):
+        '''Constructor'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+        print(self._redis.ping())
 
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
-def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    def store(self, data):
+        '''Store data in Redis'''
+        for key in data:
+            self._redis.set(key, data[key])
+        return None

8bf92551c3ff7b8ecfec5aec035d8d95e1a25c47
kingvadee <ojeifod8@gmail.com>
2024-01-25 01:37:53 +0000

    Writing strings to Redis, Reading from Redis and recovering original type, Incrementing values, Storing lists & Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module declares a redis class and methods"""
+"""Module declares  a redis class and methods"""
 import redis
 from uuid import uuid4
 from typing import Union, Callable, Optional

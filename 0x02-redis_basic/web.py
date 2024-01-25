edc74f87acc37df36abf915598c930930f71e499
kingvadee <ojeifod8@gmail.com>
2024-01-25 00:43:47 +0000

    echo

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -30,7 +30,8 @@ def count_url_access(method):
 
 
 @count_url_access
-def get_page(url: str) -> str:
+def get_page(url):
     """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
+
